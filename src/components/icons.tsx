caa0f8118198f6a4066fc775d865ba4a40d69443
Shreekrishna Bhat <shreekrishnabhat.km@gmail.com>
2025-09-21 11:06:40 +0000

    make new icon for mobile app can you do that

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,16 +8,16 @@ export const Logo = () => (
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="2.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="m5 8 6 6" />
-      <path d="m4 14 6-6 2-3" />
-      <path d="M2 5h12" />
-      <path d="m7 2 5 5" />
-      <path d="m22 22-5-10-5 10" />
-      <path d="M14 18h6" />
+      <path d="m12 3-8 18h16L12 3Z" />
+      <path d="M10 12h4" />
+      <path d="M16.5 9.5 18 8" />
+      <path d="m19 13-1.5 1.5" />
+      <path d="m5 13 1.5 1.5" />
+      <path d="M7.5 9.5 6 8" />
     </svg>
   </div>
 );
